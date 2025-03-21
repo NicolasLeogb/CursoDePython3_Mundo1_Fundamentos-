1d088335eb860f2deba2eb0d45d739721b9a45e4
nicolasleogb <nicolasleonardogomesbarbosa@gmail.com>
2025-03-21 11:58:39 -0300

    Exercício04 - Dissecando uma variável

---
+++ Exercicio4.py
@@ -0,0 +1,12 @@
+n=input('\ndigite algo: ')
+
+#Dissecando a variável
+
+print ('A classe disso é ' , type(n))
+print ('É um número? ', n. isnumeric())
+print ('É um alfabético?' , n. isalpha())
+print ('Só tem espaço?' , n. isspace())
+print ('É alpha numérico?' , n. isalnum())
+print ('Está em maiúscula?' , n. isupper())
+print ('Está em minúscula?' , n. islower())
+print ('Está capitalizada?' , n. istitle())
