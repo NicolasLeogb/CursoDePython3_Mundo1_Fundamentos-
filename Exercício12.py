c4d3c9f521c3c672aa89176d382cc0bc4c29218f
nicolasleogb <nicolasleonardogomesbarbosa@gmail.com>
2025-06-18 15:04:12 -0300

    Exercício12 - Calculando descontos

---
+++ Exercício12.py
@@ -0,0 +1,4 @@
+print('Calculadora de Descontos')
+P=float(input('Qual o preço do produto?:'))
+D = (P * 95 / 100)
+print('O produto que custava R${} , na promoção com o desconto de 5% vai custar {}'. format(P , D))
